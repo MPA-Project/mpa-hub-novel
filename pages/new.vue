f7c5027ef560c2ca0587e229f0f5f5aecdff1e46
Alfian Rikzandi <denydirgantara94@gmail.com>
2022-06-05 20:25:29 +0700

    chore: initialize text editor

---
+++ pages/new.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout name="default">
+      <main class="py-20 px-5 md:px-10 lg:px-20 max-w-[90rem] mx-auto bg1 text1 duration-200 transition-colors">
+        <div class="my-32">
+
+          <!-- Title Input -->
+          <div class="mb-8">
+            <input class="border rounded w-full py-2 px-3 leading-tight text-black" id="title" type="text" placeholder="Title" />
+          </div>
+          <!-- End Title Input -->
+
+        </div>
+      </main>
+    </NuxtLayout>
+  </div>
+</template>
